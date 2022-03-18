238d0b85922c5894fa221b5d905217d182ebf1d8
Nelson Zambrano <inge5neuromedia@gmail.com>
2022-03-17 22:53:26 -0500

    cambios juego

--- src/app/portada/portada/portada.component.ts
+++ src/app/portada/portada/portada.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-portada',
   templateUrl: './portada.component.html',
-  styleUrls: ['./portada.component.scss']
+  styleUrls: ['./portada.component.scss'],
 })
 export class PortadaComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    $('.cambiar-imagen').mouseenter(() => {
+      $('.cambiar-imagen>img').attr(
+        'src',
+        './assets/imagenes/juego2/boton-accion-portada.png'
+      );
+    });
+    $('.cambiar-imagen').mouseleave(() => {
+      $('.cambiar-imagen>img').attr(
+        'src',
+        './assets/imagenes/juego2/boton-portada.png'
+      );
+    });
   }
-
 }
